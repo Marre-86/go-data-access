f984e2a974f0c30316f5ede69113a167b6f25fee
Artem Pokhiliuk <timtimoha@yandex.ru>
2024-03-24 22:34:13 +0600

    CLI util for retreiving records from DB (PostgreSQL)

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+	"strings"
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID	int64
+	Title	string
+	Artist	string
+	Price	float32
+}
+
+func main() {
+	connString := "postgres://marre:Hombre1986@127.0.0.1:5432/recordings"
+	var err error
+	db, err = sql.Open("pgx", connString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Connected!")
+	defer db.Close()
+
+
+	fmt.Println("Choose type of search")
+	fmt.Println("1. Search by artist name")
+	fmt.Println("2. Search by album ID")
+	fmt.Println("Enter your choice (1 or 2)")
+
+	var inp int64
+	_, err = fmt.Scanln(&inp)
+	handleInputError(err)
+
+	if inp == 1 {
+		searchByArtist()
+	} else if inp == 2 {
+		searchByAlbumID()
+	} else {
+		fmt.Println("Invalid choice")
+	}
+}
+
+func searchByArtist() {
+	fmt.Println("Enter the name of the artist")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	handleInputError(err)
+	input = strings.TrimSpace(input)
+	albums, err := albumsByArtist(input)
+	fmt.Printf("Looking for albums of arist: %v\n", input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+func searchByAlbumID() {
+	fmt.Println("Enter the album ID")
+	var inp int64
+	_, err := fmt.Scanln(&inp)
+	handleInputError(err)
+	album, err := albumByID(inp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", album)
+}
+
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = $1", name)
+	if  err != nil {
+		return nil, fmt.Errorf("111albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err!= nil {
+			return  nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+	row := db.QueryRow("SELECT * FROM album WHERE id = $1", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumById %d: no such album", id)
+	}
+	return alb, nil
+}
+
+func handleInputError(err error) {
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		os.Exit(1)
+	}
+}
+
