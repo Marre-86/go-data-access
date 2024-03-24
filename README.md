00328e384e995d986c132abb7e9ffd1012bf32da
Artem Pokhiliuk <timtimoha@yandex.ru>
2024-03-25 01:40:11 +0600

    Add feature of adding records to db by user cli input

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ https://go.dev/doc/tutorial/database-access
 
 But PostgreSQL was used instead of MySQL.
 
-Also several improvements were made to make it more user-friendly (such as user prompt asking for searching parameters).
+Also several improvements were made to make it more user-friendly (such as user prompt asking for search parameters).
 
 
 In order to populate the database with dummy records from create-tables.sql you have to run
+
 `psql -U username -d recordings < create-tables.sql`
 
--- main.go
+++ main.go
@@ -34,7 +34,8 @@ func main() {
 	fmt.Println("Choose type of search")
 	fmt.Println("1. Search by artist name")
 	fmt.Println("2. Search by album ID")
-	fmt.Println("Enter your choice (1 or 2)")
+	fmt.Println("3. Add album to a database")
+	fmt.Println("Enter your choice (1 or 2 or 3)")
 
 	var inp int64
 	_, err = fmt.Scanln(&inp)
@@ -44,6 +45,8 @@ func main() {
 		searchByArtist()
 	} else if inp == 2 {
 		searchByAlbumID()
+	} else if inp == 3 {
+		insertAlbum()
 	} else {
 		fmt.Println("Invalid choice")
 	}
@@ -73,6 +76,33 @@ func searchByAlbumID() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Album found: %v\n", album)
+}
+
+func insertAlbum() {
+	fmt.Println("Enter the name of the artist")
+	reader := bufio.NewReader(os.Stdin)
+	inpArtist, err := reader.ReadString('\n')
+	handleInputError(err)
+	inpArtist = strings.TrimSpace(inpArtist)
+	fmt.Println("Enter the name of the album")
+	reader = bufio.NewReader(os.Stdin)
+	inpTitle, err := reader.ReadString('\n')
+	handleInputError(err)
+	inpTitle = strings.TrimSpace(inpTitle)
+	var inpPrice float32
+	fmt.Println("Enter the price")
+	_, err = fmt.Scanf("%f", &inpPrice)
+	handleInputError(err)
+
+	albID, err := addAlbum(Album{
+		Title:	inpTitle,
+		Artist:	inpArtist,
+		Price: inpPrice,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Success! ID of added album: %v\n", albID)
 }
 
 
@@ -107,6 +137,15 @@ func albumByID(id int64) (Album, error) {
 		return alb, fmt.Errorf("albumById %d: no such album", id)
 	}
 	return alb, nil
+}
+
+func addAlbum(alb Album) (int64, error) {
+	var id int64
+	err := db.QueryRow("INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id", alb.Title, alb.Artist, alb.Price).Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
 
 func handleInputError(err error) {
